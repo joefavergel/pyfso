247d2a385161bef2f376246c281ed3e588122565
Joseph Vergel-Becerra <joseph@guane.com.co>
2021-06-30 22:04:52 -0500

    :sparkles: Bessel Iv calculation added

--- pyfso/oam/bessel_gauss.py
+++ pyfso/oam/bessel_gauss.py
@@ -6,6 +6,8 @@ from scipy import special
 
 from pyfso.oam.base import BaseBeam
 
+plt.style.use("science")
+
 
 class BesselGauss(BaseBeam):
     def __init__(
@@ -16,7 +18,7 @@ class BesselGauss(BaseBeam):
         angular_momentum: float = 5.0,
         beam_waist: float = (0.57e-3) / 2.0,
         z_coor: float = 0.0,
-        kind: str = "J",
+        kind: str = "Jv",
     ):
         """Bessel-Gauss Beam with Jv(z) Function
 
@@ -81,26 +83,65 @@ class BesselGauss(BaseBeam):
 
     def create(self):
         j = 0.0 + 1.0j
-        self.E = np.multiply(
-            special.jv(self.angular_momentum, self.kt * self.rho),
-            np.multiply(
-                np.exp(j * self.angular_momentum * self.phi),
-                np.exp(np.multiply(-self.rho, self.rho) / (self.beam_width ** 2)),
-            ),
-        )
+        if self.kind == "Jv":
+            self.E = np.multiply(
+                special.jv(self.angular_momentum, self.kt * self.rho),
+                np.multiply(
+                    np.exp(j * self.angular_momentum * self.phi),
+                    np.exp(np.multiply(-self.rho, self.rho) / (self.beam_width ** 2)),
+                ),
+            )
+        elif self.kind == "Iv":
+            n = 1.000293  # refractive index
+            d = 1.0
+            thetaG = 0.0002  # beam divergence in [rad]
+            self.beam_waist / d
+            self.wave_number * np.sin(self.wave_length / (d * thetaG * np.pi * n))
+            coff = (
+                (np.sqrt(np.pi) / self.beam_waist)
+                * (np.exp(j * (self.z_coor / self.rayleigh_range)))
+                * (np.exp(j * (3 * np.pi / 2) * self.angular_momentum))
+                * (np.exp(j * self.wave_number * self.z_coor))
+            )
+            bessel_iv = special.iv(
+                (self.angular_momentum - 1) / 2,
+                (self.rho ** 2) / (2 * (self.beam_width ** 2)),
+            ) - special.iv(
+                (self.angular_momentum + 1) / 2,
+                (self.rho ** 2) / (2 * (self.beam_width ** 2)),
+            )
+            self.E = coff * np.multiply(
+                bessel_iv,
+                np.multiply(
+                    self.rho,
+                    np.multiply(
+                        np.exp(j * self.angular_momentum * self.phi),
+                        np.exp(-(self.rho ** 2) / (self.beam_width ** 2)),
+                    ),
+                ),
+            )
+        else:
+            raise ValueError(
+                "The Bessel-Gauss kind attribute introduced is unkown or unsupported."
+            )
 
     def compute_intensity(self):
         intensity = np.multiply(self.E, np.conj(self.E))
         return intensity
 
     def plot_oam(self):
-        plt.figure(figsize=(20, 20))
-        intensity = self.compute_intensity()
-        plt.imshow(np.real(intensity), extent=[0, 1, 0, 1], cmap="inferno")
-        plt.title(
-            r"Bessel-Gauss Beam to First Diffraction Order with $m={am}$".format(
-                am=self.angular_momentum
+        with plt.style.context(["dark_background", "science", "high-vis"]):
+            plt.figure(figsize=(12, 12))
+            intensity = self.compute_intensity()
+            plt.imshow(np.real(intensity), extent=[0, 1, 0, 1], cmap="inferno")
+            plt.title(
+                r"Bessel-Gauss ${kind_}(z)$ Beam to First Diffraction Order with $m={am}$".format(
+                    am=self.angular_momentum, kind_=self.kind
+                ),
+                fontsize=24,
             )
-        )
-        plt.xlim(0.3, 0.7)
-        plt.ylim(0.3, 0.7)
+            plt.xlim(0.3, 0.7)
+            plt.ylim(0.3, 0.7)
+            plt.xticks(fontsize=16)
+            plt.yticks(fontsize=16)
+            plt.show()
